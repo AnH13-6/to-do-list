48fe51188e3301164fd1d59177177a6a6d52a185
Anne Hölscher <anne.k.hoelscher@gmail.com>
2022-09-11 19:55:21 +0200

    add edit function

---
+++ pages/GlobalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+  * {
+    padding: 3px;
+    box-sizing: border-box;
+    font-family: Roboto;
+    flex-direction: row;
+  }
+
+`;
+
+export default GlobalStyle;
